b99bdfe6e03631179d5b2b8a49bea50a69781b5e
tangyulong <tang@123.com>
2022-11-07 16:12:20 +0800

    hot-fix second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,7 +3,9 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-
+        System.out.println("hello update");
+        System.out.println("master third");
+        System.out.println("hot-fix second");
         System.out.println("hot-fix");
     }
 }
