ec4b4078812de437def6b51fa5be66e58f5e4702
tangyulong <tang@123.com>
2022-11-07 16:10:47 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello update");
+        System.out.println("master third");
 
         System.out.println("hot-fix");
     }
